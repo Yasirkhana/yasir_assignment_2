8390d0cce77918e97440214e99619e714284b9c8
Yasir Ahmad <khan10055yasir@gmail.com>
2021-05-30 10:27:19 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,19 @@ void main(){
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body:  Container(
+          height: 100,
+          width: 100,
+          color: Colors.red,
+        )
+        )
+        
+      );
+    
+  }
+}
